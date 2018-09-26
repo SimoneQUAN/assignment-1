a36195f58085e075fefb50e2f1ce1d01f9781c60
simoneq <yezi_chibang@sina.com>
2018-09-26 20:35:03 +0200

    exo 4 finished

--- functions.R
+++ functions.R
@@ -9,13 +9,19 @@
 # if the specified column exists and contains numbers, returns the sum of
 # all values in the column; otherwise, returns NULL
 sum_column <- function(d, var) {
+  # set a defaut value to return
   result <- NULL
+  # access a column
   x <- d[[var]] 
+  # tests whether the column exists in d
   if (!is.null(x)) {
+    # if x contains numbers
     if (is.numeric(x)) {
+      #, set the variable to be the sum of the values in x
       result <- sum(x)
     }
   }
+  # return the result
   return(result)
 }
 
@@ -90,8 +96,34 @@ sum_divided_by <- function(x, k) {
 #
 # [YOUR FUNCTION HERE]
 my_mean <- function(x) {
+  # if the result of my_sum(x) is not NULL
   if (!is.null(my_sum(x))) {
+    # return the result of sum_divided_by(x,len(x))
     return(sum_divided_by(x, length(x)))
   }
+  # otherwise, return NULL
   return(NULL)
+}
+
+
+# function for Sub-exercise 3-d
+# Return a violin plot.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the dependent variable, provided as a
+#     string
+# grouping_var: the name of a column of d containing a grouping variable,
+#               provided as a string
+#
+# RETURN VALUE:
+# A ggplot plot object containing a violin plot, grouped by the values
+# of the grouping variable.
+#
+grouped_violin_plot <- function(d, var, grouping_var) {
+  # Create the base ggplot object
+  p <- ggplot2::ggplot(d, ggplot2::aes_string(y=var, x=grouping_var, fill=grouping_var))
+  # YOUR CODE HERE: Create a violin plot
+  p <- p + ggplot2::geom_violin()
+  return(p) 
 }
